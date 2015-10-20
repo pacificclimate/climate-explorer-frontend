57074aafeb9238c0930ee6442d2058a185258d4d
Basil Veerman <bveerman@uvic.ca>
2015-10-19 17:57:11 -0700

    Pull front end out of flask back end

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
-module.exports = {
-    entry : {
-        index : "./index.js"
-    },
-    output : {
-        path : "./build",
-        filename : "bundle.js"
-    },
-    module : {
-        loaders :[
-            {test:/\.js$/, loader:'jsx-loader'}
-        ]
+'use strict';
 
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+      'webpack/hot/only-dev-server',
+      path.resolve(__dirname, 'src', 'index.js')
+    ],
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+
+    module: {
+        loaders: [
+            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+	colors: true
     }
-}
+};
