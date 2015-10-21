c44c660d11357bd849ad651b97547e187ca133b4
Basil Veerman <bveerman@uvic.ca>
2015-10-21 13:02:54 -0700

    Add postcss support, autoprefixer options

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@
 
 var path = require('path');
 var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const AUTOPREFIXER_BROWSERS = [
+  'Chrome >= 35',
+  'Firefox >= 31',
+  'Explorer >= 9',
+  'Opera >= 12',
+  'Safari >= 7.1'
+];
 
 module.exports = {
     entry: [
@@ -42,8 +49,11 @@ module.exports = {
         new ExtractTextPlugin('style.css', { allChunks: true }),
     ],
 
-    postcss: function () {
-        return [autoprefixer];
+    postcss: function plugins(bundler) {
+        return [
+            require('postcss-import')({ addDependencyTo: bundler }),
+            require('postcss-cssnext')({ autoprefixer: AUTOPREFIXER_BROWSERS })
+        ];
     },
 
     devServer: {
