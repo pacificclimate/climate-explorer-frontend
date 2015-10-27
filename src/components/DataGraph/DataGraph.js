469e3e56a2bb75515f802ddc65e0d9c23cdc1d8c
Michael Fischer <mfischer@uvic.ca>
2015-10-27 16:32:12 -0700

    Adds model data parsing for DataGraph as per backend API.

--- src/components/DataGraph/DataGraph.js
+++ src/components/DataGraph/DataGraph.js
@@ -2,30 +2,70 @@ import React, { PropTypes, Component } from 'react';
 //import Rickshaw from 'rickshaw';
 var Rickshaw = require("rickshaw/rickshaw");
 
+import styles from './DataGraph.css';
 
 var DataGraph = React.createClass({
 
+    parseData: function(data) {
+        var allModelsData = [];
+        for (let model in data) {
+
+            var modelData = []
+            for (let key in data[model]) {
+                if (parseInt(key)) {
+                    var val = data[model][key];
+                    modelData.push({x: parseInt(key), y: val});
+                }
+            }
+
+            // sort model data along x axis
+            allModelsData.push({
+                color: 'steelblue', // generate dynamically
+                data:modelData
+            });
+        }
+        return allModelsData;
+    },
+
+
     componentDidMount: function() {
+
+        var seriesData = this.parseData(this.props.data);
+
         var graph = new Rickshaw.Graph( {
-            element: this.getDOMNode(),
-            renderer: 'scatterplot',
+            element: document.getElementById('graph'),
+            renderer: 'lineplot',
             interpolation: 'linear',
-            series: [
-    {
-      color: 'steelblue',
-      data: [ { x: 0, y: 80}, { x: 1, y: 15 }, { x: 2, y: 79 } ]
-    }, {
-      color: 'lightblue',
-      data: [ { x: 0, y: 30}, { x: 1, y: 20 }, { x: 2, y: 64 } ]
-    }
-  ]
+            min: 'auto',
+            padding: {
+                top: 0.1,
+                bottom: 0.1,
+                right: 0.1,
+                left: 0.1
+            },
+            series: seriesData
         });
-      graph.render();
+
+        var y_ticks = new Rickshaw.Graph.Axis.Y( {
+            graph: graph,
+            orientation: 'left',
+            tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+            element: document.getElementById('y_axis')
+        } );
+
+        var graphHover = new Rickshaw.Graph.HoverDetail({
+            graph:graph
+        });
+
+        graph.render();
     },
 
     render: function () {
         return (
-            <div></div>
+            <div>
+            <div id={'y_axis'} className={styles.y_axis}></div>
+            <div id={'graph'} className={styles.container}></div>
+            </div>
         )
     }
 });
--- src/components/DataGraph/GraphOverlay.js
+++ src/components/DataGraph/GraphOverlay.js
@@ -2,41 +2,49 @@ import React, { PropTypes, Component } from 'react';
 
 import PanelOverlay from '../PanelOverlay/PanelOverlay';
 import DataGraph from './DataGraph';
+
+import styles from './GraphOverlay.css';
+
 const testData = {
     "model_id1": {
         "units": "degC",
-        "2050": 20.0,
+        "2050": 21.0,
         "2080": 35.0,
-        "2020": 10.0
+        "2020": 11.0
     },
     "model_id2": {
         "units": "degC",
-        "2050": 20.0,
-        "2080": 35.0,
-        "2020": 10.0  
+        "2050": 22.0,
+        "2080": 36.0,
+        "2020": 12.0  
     },
     "model_id3": {
         "units": "degC",
-        "2050": 20.0,
-        "2080": 35.0,
-        "2020": 10.0  
+        "2050": 23.0,
+        "2080": 37.0,
+        "2020": 13.0  
     },
     "model_id4": {
         "units": "degC",
-        "2050": 20.0,
-        "2080": 35.0,
-        "2020": 10.0  
+        "2050": 24.0,
+        "2080": 39.0,
+        "2020": 14.0  
     }
 };
 
-        // <PanelOverlay title={'Data Plot'} maxHeight={600} align={'right'}>
-        //     <DataGraph data={testData} />
-        // </PanelOverlay>
-
 class GraphOverlay extends Component {
+
+    // getDefaultProps () {
+    //     return {
+    //         maxHeight: 600,
+    //         maxWidth: 1000,
+    //         align: 'right',
+    //     };
+    // }
+
     render () {
-        return (
-        <PanelOverlay title={'Data Plot'} maxHeight={600} align={'right'}>
+        return (        
+        <PanelOverlay title={'Data Plot'} maxHeight={500} maxWidth={500} align={'right'}>
         <DataGraph data={testData} />
         </PanelOverlay>
         )
