6bd3ddca3bc65ce680862b05c95aea86602ed458
James Hiebert <james@hiebert.name>
2015-11-27 13:45:24 -0800

    Eliminated a bit of redundancy in the even handling for the AppController selectors

--- src/components/AppController/AppController.js
+++ src/components/AppController/AppController.js
@@ -72,40 +72,30 @@ var App = React.createClass({
           meta: models
         })
       }
-
     }.bind(this));
-    
   },
 
   updateSelection: function(param, selection) {
     var update = {}; update[param] = selection;
     this.setState(update);
   },
-  
+
   render: function() {
-    var getModels = function() {
-      return _.unique(this.state.meta.map(function(el){return el['model_id']}))
+    var getThings = function(thing) {
+      return _.unique(this.state.meta.map(function(el){return el[thing]}))
     }.bind(this);
-
-    var getVariables = function() {
-      return _.unique(this.state.meta.map(function(el){return el['variable_id']}))
-    }.bind(this)
-
-    var getScenarios = function() {
-      return _.unique(this.state.meta.map(function(el){return el['experiment']}))
-    }.bind(this)
 
     return (
       <Grid fluid={true}>
         <Row>
           <Col lg={4}>
-            <Selector label={"Model Selection"} onChange={this.updateSelection.bind(this, 'model_id')} items={getModels()}/>
+            <Selector label={"Model Selection"} onChange={this.updateSelection.bind(this, 'model_id')} items={getThings('model_id')}/>
           </Col>
           <Col lg={4}>
-            <Selector label={"Variable Selection"} onChange={this.updateSelection.bind(this, 'variable_id')} items={getVariables()}/>
+            <Selector label={"Variable Selection"} onChange={this.updateSelection.bind(this, 'variable_id')} items={getThings('variable_id')}/>
           </Col>
           <Col lg={4}>
-            <Selector label={"Emission Scenario Selection"} onChange={this.updateSelection.bind(this, 'experiment')} items={getScenarios()}/>
+            <Selector label={"Emission Scenario Selection"} onChange={this.updateSelection.bind(this, 'experiment')} items={getThings('experiment')}/>
           </Col>
         </Row>
         <Row>
