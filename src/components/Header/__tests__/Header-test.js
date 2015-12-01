9fd1d164f67f336db8da1f54611dc66f146d3b94
Basil Veerman <bveerman@uvic.ca>
2015-12-01 08:44:20 -0800

    Add rendering test for header component

---
+++ src/components/Header/__tests__/Header-test.js
@@ -0,0 +1,16 @@
+jest.dontMock('../Header');
+jest.dontMock('react-bootstrap');
+
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const Header = require('../Header');
+
+describe('Header', function() {
+
+  it('renders', function(){
+    var header = TestUtils.renderIntoDocument(<Header />);
+    expect(TestUtils.isCompositeComponent(header)).toBeTruthy()
+  });
+});
