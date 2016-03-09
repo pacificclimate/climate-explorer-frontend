3a366b5b91c09c660b0b30a21f680b92bbcd163f
James Hiebert <hiebert@uvic.ca>
2016-03-09 13:49:19 -0800

    Fixes Selector to not require default value prop
    The last commit had an unfortunate side effect that caused Selector
    components to always show the first element as being selected if
    a value wasn't passed in as props.

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -16,7 +16,7 @@ var Selector = React.createClass({
 
   render: function() {
     return (
-      <Input type="select" label={this.props.label} onChange={this.handleChange} value={this.props.value}>
+      <Input type="select" label={this.props.label} onChange={this.handleChange} value={this.props.value ? this.props.value : undefined}>
       {
         this.props.items.map(function(item) {
           return Array.isArray(item) ? <option value={item[0]} key={item[0]}>{item[1]}</option> : <option value={item} key={item}>{item}</option>;
