f4794a4b1d3890cb515a2e06facaa0ada1c4acc2
corviday <corvi@clockworksquid.com>
2017-07-06 16:17:01 -0700

    I apologize if you are getting emailed this spam.

--- src/components/Header/__tests__/Header-test.js
+++ src/components/Header/__tests__/Header-test.js
@@ -1,7 +1,7 @@
 jest.dontMock('../Header');
 jest.dontMock('react-bootstrap');
 
-//comment changed++
+//comment changed+++
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
