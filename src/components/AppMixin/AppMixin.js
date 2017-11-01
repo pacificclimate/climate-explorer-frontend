2187ad290717e40b862a47f931df0f4f407f856f
Lee Zeman <lzeman@uvic.ca>
2017-11-01 13:04:35 -0700

    Available datasets filterable by portal

--- src/components/AppMixin/AppMixin.js
+++ src/components/AppMixin/AppMixin.js
@@ -40,13 +40,17 @@ var AppMixin = {
             var end = response.data[key].end_date;
             end = moment(end, moment.ISO_8601).utc().format('YYYY');
 
-            models.push(_.extend({
-              unique_id: key,
-              variable_id: vars[v],
-              start_date: start,
-              end_date: end,
-              variable_name: response.data[key].variables[vars[v]],
-              }, _.omit(response.data[key], 'variables', 'start_date', 'end_date')));
+            //If this app has a dataset filter defined, filter the data
+            if(typeof this.datasetFilter == "undefined" ||
+                this.datasetFilter(response.data[key])) {
+              models.push(_.extend({
+                unique_id: key,
+                variable_id: vars[v],
+                start_date: start,
+                end_date: end,
+                variable_name: response.data[key].variables[vars[v]],
+                }, _.omit(response.data[key], 'variables', 'start_date', 'end_date')));
+              }
             }
           }
 
@@ -72,14 +76,7 @@ var AppMixin = {
   },
 
   getVariableIdNameArray: function () {
-    var descriptions = {};
-    _.each(this.state.meta, m => {
-      var variable = m.variable_id;
-      descriptions[variable] = this.getMergedVariableDescription(variable) ?
-          this.getMergedVariableDescription(variable) : m.variable_name;
-    });
-
-    var varArray = _.zip(_.keys(descriptions), _.values(descriptions));
+    var varArray = _.zip(this.getMetadataItems('variable_id'), this.getMetadataItems('variable_name'));
     var varNames = _.map(varArray, function (v) {
       return v[0] + ' - ' + v[1];
     });
@@ -87,42 +84,8 @@ var AppMixin = {
       return a[0] > b[0] ? 1 : -1;
     });
     return varOptions;
-
-  /*****************************************************************
-   * This is the old functionality of getVariableIdNameAray, which
-   * is more straightforward and efficient. It should be restored
-   * when the metadata is fixed to remove variable_ids with
-   * conflicting descriptions.
-   *****************************************************************
-   * var varArray = _.zip(this.getMetadataItems('variable_id'), this.getMetadataItems('variable_name'));
-   * var varNames = _.map(varArray, function (v) {
-   *   return v[0] + ' - ' + v[1];
-   * });
-   * var varOptions = _.zip(this.getMetadataItems('variable_id'), varNames).sort(function (a, b) {
-   *   return a[0] > b[0] ? 1 : -1;
-   * });
-   * return varOptions;
-   *******************************************************************/
   },
-
-  //This function is a kludge to resolve variable_ids associated with more 
-  //than one description. It provides a canonical description for
-  //cases where multiple descriptions are associated with a single variable.
-  //Typically this applies to indices calculated seperately over differnt
-  //timescales, but accidentally assigned the same variable_id.
-  //This function should be removed when it is no longer necessary.
-  getMergedVariableDescription: function (variable) {
-    var descriptions = {
-        "rx1dayETCCDI":  "Maximum 1-day Precipitation",
-        "txxETCCDI": "Maximum of Daily Maximum Temperature",
-        "tnxETCCDI": "Maximum of Daily Minimum Temperature",
-        "rx5dayETCCDI": "Maximum Consecutive 5-day Precipitation",
-        "txnETCCDI": "Minimum of Daily Maximum Temperature",
-        "tnnETCCDI": "Minimum of Daily Minimum Temperature"
-    };
-    return descriptions[variable];
-  },
-
+  
   updateSelection: function (param, selection) {
     var update = {}; update[param] = selection;
     this.setState(update);
