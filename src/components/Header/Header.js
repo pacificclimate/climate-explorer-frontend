b254b874040a30e3473695c1d3972920eb689fb9
James Hiebert <james@hiebert.name>
2017-12-01 10:58:00 -0800

    Converts page navigation from Buttons to Nav tabs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Button } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
+import { browserHistory } from 'react-router';
 import classNames from 'classnames';
 
 import styles from './Header.css';
@@ -8,6 +8,11 @@ import styles from './Header.css';
 class Header extends Component {
 
   render() {
+
+    var handleSelect = function() {
+      browserHistory.push(this.href);
+    };
+
     return (
       <div className={classNames(styles.header)}>
         <Grid fluid>
@@ -23,20 +28,12 @@ class Header extends Component {
               </a>
             </Col>
             <Col lg={8}>
-              <Row>
-                <Link to="/climo/all_downscale_files" activeClassName="active">
-                  <Button bsStyle="primary">Standard climatologies</Button>
-                </Link>
-                <Link to="/climo/all_CLIMDEX_files" activeClassName="active">
-                  <Button bsStyle="primary">ClimDEX climatologies</Button>
-                </Link>
-                <Link to="/compare/all_downscale_files" activeClassName="active">
-                  <Button bsStyle="primary">Standard comparision</Button>
-                </Link>
-                <Link to="/compare/all_CLIMDEX_files" activeClassName="active">
-                  <Button bsStyle="primary">ClimDEX comparision</Button>
-                </Link>
-              </Row>
+              <Nav bsStyle="tabs" justified activeKey={1} onSelect={handleSelect}>
+                <NavItem eventKey={1} href="/climo/all_downscale_files">Standard climatologies</NavItem>
+                <NavItem eventKey={2} href="/climo/all_CLIMDEX_files">ClimDEX climatologies</NavItem>
+                <NavItem eventKey={3} href="/compare/all_downscale_files">Standard comparison</NavItem>
+                <NavItem eventKey={4} href="/compare/all_CLIMDEX_files">ClimDEX comparison</NavItem>
+              </Nav>
             </Col>
           </Row>
         </Grid>
