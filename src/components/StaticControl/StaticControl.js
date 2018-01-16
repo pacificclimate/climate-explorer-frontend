f83669f3fbb95f7b3c7cdd5309b52762f6ea449f
Rod Glover <rglover@uvic.ca>
2018-01-16 11:30:52 -0800

    Add StaticControl component

---
+++ src/components/StaticControl/StaticControl.js
@@ -0,0 +1,28 @@
+import ReactDom from 'react-dom';
+
+import { MapControl } from 'react-leaflet';
+import L from 'leaflet';
+
+import './StaticControl.css';
+
+class StaticControl extends MapControl {
+    createLeafletElement(props) {
+        const leafletElement = L.control();
+
+        leafletElement.onAdd = map => {
+            this.container = L.DomUtil.create('div', 'StaticControl leaflet-control');
+             ReactDom.render(props.children, this.container);
+            return this.container;
+        };
+
+        return leafletElement;
+    }
+
+    updateLeafletElement (fromProps, toProps) {
+        if (fromProps.children !== toProps.children) {
+            ReactDom.render(toProps.children, this.container);
+        }
+    }
+}
+
+export default StaticControl;
