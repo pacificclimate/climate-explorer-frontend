c23350fb867a58d31bf8769a763bfe39fd5a9c54
Rod Glover <rglover@uvic.ca>
2018-01-17 15:54:10 -0800

    Add buttonWithModal HOC

---
+++ src/HOCs/buttonWithModal.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+
+function buttonWithModal(ButtonContent, ModalBody) {
+  return class extends React.Component {
+    static propTypes = {
+    };
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        showModal: false,
+      };
+    }
+
+    openModal = () => {
+      this.setState({ showModal: true });
+    };
+
+    closeModal = () => {
+      this.setState({ showModal: false });
+    };
+
+    render() {
+      return (
+        <div>
+          <Button onClick={this.openModal}>
+            <ButtonContent/>
+          </Button>
+
+          <Modal show={this.state.showModal} onHide={this.closeModal}>
+            <ModalBody/>
+          </Modal>
+        </div>
+      );
+    }
+  };
+}
+
+export default buttonWithModal;
