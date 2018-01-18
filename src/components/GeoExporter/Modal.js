283f0edf4403be190d0dffa700fedadce71e2bfd
Rod Glover <rglover@uvic.ca>
2018-01-18 10:08:00 -0800

    Refactor GeoExporter.Modal to use HOC compAcontrolsB

--- src/components/GeoExporter/Modal.js
+++ src/components/GeoExporter/Modal.js
@@ -3,55 +3,71 @@ import React from 'react';
 import { ButtonGroup, Button, Glyphicon, Modal } from 'react-bootstrap';
 
 import g from '../../core/geo';
-import buttonWithModal from '../../HOCs/buttonWithModal';
+import compAcontrolsB from '../../HOCs/compAcontrolsB';
 
 
-var exportPolygon = function (area, format) {
-  // this.closeModal();
+var exportPolygon = function (area, format, close) {
+  close();
   g.geojson(area).save(format);
 };
 
 
-class GeoExporterModal extends React.Component {
+export default class GeoExporterModal extends React.Component {
   // This class composes the button content with the modal content using
-  // the HOC `buttonWithModal`. Doing this was instructive; it's not quite
+  // the HOC `compAcontrolsB`. Doing this was instructive; it's not quite
   // as simple as it might seem. For details, the documentation in
-  // `buttonWithModal`.
+  // `compAcontrolsB`.
 
   static propTypes = {
     area: PropTypes.object,
     title: PropTypes.string,
   };
 
-  ButtonBody = () => <Glyphicon glyph='save-file'/>;
+  A = (props) =>
+    <Button onClick={props.open}>
+      <Glyphicon glyph='save-file'/>
+    </Button>
+  ;
 
-  // TODO: Tighten this rubbish up. Good grief.
-  exportShapefile = () => exportPolygon.bind(this, this.props.area, 'shp')();
-  exportGeoJSON = () => exportPolygon.bind(this, this.props.area, 'geojson')();
-  exportWKT = () => exportPolygon.bind(this, this.props.area, 'wkt')();
-  exportKML = () => exportPolygon.bind(this, this.props.area, 'kml')();
-  exportGPX = () => exportPolygon.bind(this, this.props.area, 'gpx')();
 
-  ModalBody = () => <div>
-    <Modal.Header closeButton>
-      <Modal.Title>Export Polygon by Type</Modal.Title>
-    </Modal.Header>
+  B = (props) => {
+    // TODO: Create these by mapping over file type. Not so easy as it seems.
+    const exportShapefile =
+      exportPolygon.bind(this, this.props.area, 'shp', props.close);
+    const exportGeoJSON =
+      exportPolygon.bind(this, this.props.area, 'geojson', props.close);
+    const exportWKT =
+      exportPolygon.bind(this, this.props.area, 'wkt', props.close);
+    const exportKML =
+      exportPolygon.bind(this, this.props.area, 'kml', props.close);
+    const exportGPX =
+      exportPolygon.bind(this, this.props.area, 'gpx', props.close);
 
-    <Modal.Body>
-      <ButtonGroup>
-        <Button onClick={this.exportShapefile}>Shapefile</Button>
-        <Button onClick={this.exportGeoJSON}>GeoJSON</Button>
-        <Button onClick={this.exportWKT}>WKT</Button>
-        <Button onClick={this.exportKML}>KML</Button>
-        <Button onClick={this.exportGPX}>GPX</Button>
-      </ButtonGroup>
-    </Modal.Body>
-  </div>;
+    return (
+      <Modal show={props.show} onHide={props.close}>
+        <Modal.Header closeButton>
+          <Modal.Title>Export Polygon by Type</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <ButtonGroup>
+            <Button onClick={exportShapefile}>Shapefile</Button>
+            <Button onClick={exportGeoJSON}>GeoJSON</Button>
+            <Button onClick={exportWKT}>WKT</Button>
+            <Button onClick={exportKML}>KML</Button>
+            <Button onClick={exportGPX}>GPX</Button>
+          </ButtonGroup>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={props.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
 
   render() {
-    const ButtonWithModal = buttonWithModal(this.ButtonBody, this.ModalBody);
+    const ButtonWithModal = compAcontrolsB(this.A, this.B);
     return <ButtonWithModal/>;
   }
 }
-
-export default GeoExporterModal;
