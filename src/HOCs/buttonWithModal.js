1f982fbd10409289248ea04406d5f7972ab5d4cc
Rod Glover <rglover@uvic.ca>
2018-01-17 17:30:55 -0800

    Add documentation to buttonWithModal

--- src/HOCs/buttonWithModal.js
+++ src/HOCs/buttonWithModal.js
@@ -1,3 +1,31 @@
+// HOC yielding a component that contains a button that opens a modal dialog.
+//
+// Parameters are the content of the button (e.g., text or Glyphicon) and
+// the body of the modal dialog (which may contain multiple modal dialog
+// subparts but must constitute a single React element; fortunately you can
+// use a div for this).
+//
+// Using this HOC in real application is not quite as simple as it might seem.
+// Specifically:
+//
+//  - If you want to use props (or state) in the component returned by
+//    a HOC, you have to wrap the result of the HOC in a component that
+//    defines the props (or state), and render the result of the HOC.
+//
+//  - This enables you to define components that use those props and state
+//    to passed to the HOC.
+//
+//  - The components passed to the HOC must be *components*, not
+//    not instantiations (objects); you'll need to define functional components
+//    `ButtonContent` and `ModalBody` on the class, and pass those functions
+//    as arguments to the HOC. Code fragment:
+//
+//      ButtonWithModal = buttonWithModal(this.ButtonContent, this.ModalBody);
+//
+//  - This is all a little complex, but logical. Is there a better way?
+//
+// For an example use case, see component GeoExporter.Modal.
+
 import React from 'react';
 import { Button, Modal } from 'react-bootstrap';
 
