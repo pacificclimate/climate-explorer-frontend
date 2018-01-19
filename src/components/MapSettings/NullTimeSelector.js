18a25e708f61bb908bbdc2cbd675912bf5632e30
Rod Glover <rglover@uvic.ca>
2018-01-18 16:48:04 -0800

    Define MapSettings NullTimeSelector

---
+++ src/components/MapSettings/NullTimeSelector.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Selector } from '../Selector';
+
+export default function NullTimeSelector() {
+  return (
+    <Selector
+      label="Year and Time of Year"
+      disabled
+    />
+  );
+}
