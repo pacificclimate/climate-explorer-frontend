9d70e7c74c2ad184b7ff65eff29b74a6574f303b
Rod Glover <rglover@uvic.ca>
2018-01-18 16:48:59 -0800

    Define MapSettings DataDisplayControls with real TimeSelector and
    skeleton PaletteSelector and ScaleSelector

---
+++ src/components/MapSettings/DataDisplayControls.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import _ from 'underscore';
+import TimeSelector from './TimeSelector';
+import PaletteSelector from './PaletteSelector';
+import ScaleSelector from './ScaleSelector';
+
+
+export default class DataDisplayControls extends React.Component {
+  static propTypes = {
+    name: PropTypes.string, // 'Raster' | 'Isoline'
+    disabled: PropTypes.bool,
+    times: PropTypes.object,
+    timeIdx: PropTypes.string,
+    onChangeTime: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <TimeSelector
+          name={this.props.name}
+          disabled={this.props.disabled}
+          times={this.props.times}
+          timeIdx={this.props.timeIdx}
+          onChange={this.props.onChangeTime}
+        />
+        <PaletteSelector
+          name={this.props.name}
+        />
+        <ScaleSelector
+          name={this.props.name}
+        />
+      </div>
+    );
+  }
+}
