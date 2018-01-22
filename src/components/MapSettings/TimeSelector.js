c134359ba7b4b23a1c4e2e82ea47ec73bfe93da1
Rod Glover <rglover@uvic.ca>
2018-01-22 13:21:08 -0800

    Clean up TimeSelector code

--- src/components/MapSettings/TimeSelector.js
+++ src/components/MapSettings/TimeSelector.js
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import _ from 'underscore';
-import {sameYear, timestampToTimeOfYear} from '../../core/util';
+
+import { sameYear, timestampToTimeOfYear } from '../../core/util';
 import NullTimeSelector from './NullTimeSelector';
 import Selector from '../Selector';
 
@@ -14,57 +15,6 @@ export default class TimeSelector extends React.Component {
     timeIdx: PropTypes.string,
     onChange: PropTypes.func.isRequired,
   };
-
-  // From context of MapController
-  // makeTimeSelector (symbol) {
-  //   var times = this.state[`${symbol}Times`]
-  //
-  //   if(_.isUndefined(times)) {
-  //     //metadata API call hasn't finished loading yet; return disabled selector.
-  //     //(user shouldn't see this unless something is off with backend -
-  //     // metadata query should be loaded by the time the user opens this menu.)
-  //     return (
-  //       <Selector
-  //         label={'Year and Time of Year'}
-  //         disabled={true}
-  //       />
-  //     );
-  //   }
-  //
-  //   var timeList = _.values(times);
-  //   var disambiguateYears = !sameYear(_.first(timeList), _.last(timeList));
-  //   var labelText = disambiguateYears ? 'Year and Time of Year' : 'Time of Year';
-  //
-  //   //user has chosen to link up times across the two variables, so
-  //   //disable the comparand time selector.
-  //   if(this.state.linkTimes && symbol == 'comparand') {
-  //     var selector = (
-  //       <Selector
-  //         label={labelText}
-  //         disabled={true}
-  //       />
-  //     );
-  //     selector = this.addTooltipWrapper(selector, 'Timestamp matching is activated', 'right');
-  //     return selector;
-  //   }
-  //
-  //   var timeOptions = _.map(times, function (v, k) {
-  //     return [k, timestampToTimeOfYear(v, JSON.parse(k).timescale, disambiguateYears)];
-  //   });
-  //
-  //
-  //   labelText = `${this.userLabels[symbol]} ${labelText}`;
-  //   var value = this.state[`${symbol}TimeIdx`];
-  //
-  //   return (
-  //     <Selector
-  //       label={labelText}
-  //       onChange={this.updateTime.bind(this, symbol)}
-  //       items={timeOptions}
-  //       value={value}
-  //     />
-  //   );
-  // }
 
   disambiguateYears() {
     const timeList = _.values(this.props.times);
@@ -87,9 +37,6 @@ export default class TimeSelector extends React.Component {
   }
 
   render() {
-    // rasterTimeSelector = this.makeTimeSelector('variable');
-    // isolineTimeSelector = this.makeTimeSelector('comparand');
-
     if (!this.props.times) {
       // Code smell:
       // metadata API call hasn't finished loading yet; return disabled selector.
