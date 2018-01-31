26ef01a1cd7ece78b0987a84efdb6ebe207c7826
Rod Glover <rglover@uvic.ca>
2018-01-31 14:28:16 -0800

    Make compAcontrolsB pass this.props to wrapped components

--- src/HOCs/compAcontrolsB.js
+++ src/HOCs/compAcontrolsB.js
@@ -1,41 +1,36 @@
-// HOC that composes two components, A and B (plus a wrapper component), such
-// that A "controls" B in the sense that A triggers state changes that cause B
-// to be in open/closed state.
+// HOC that composes two components, `A` and `B` (plus a wrapper component),
+// such that `A` "controls" `B` in the sense that `A` triggers state changes
+// that cause `B` to be in open/closed (show/hide) state.
 //
 // The canonical example of this is a button that opens a dialog.
 //
-// Components A and B are wrapped in component Wrapper, which defaults to div.
-// It may be better to make the default be the new React fragment component.
+// The purpose of this HOC is to encapsulate the injection of the open/closed
+// state and callbacks into the controlling and controlled components.
 //
-// Note that B is also passed the state-controlling callbacks so that it
-// can close (or open) itself if need be. However, B is not considered the
+// Components `A` and `B` are passed the following props:
+//
+// - All props passed to the component returned by the HOC, so that A and B can
+//    construct content based on those props, necessary for many use cases.
+//
+// - Three new values set up here: `show`, `open`, `close`, to control the
+//    behaviour of the controlled component B.
+//
+// Note: `B` (as well as `A`) is passed the state-control callbacks so that it
+// can control itself if need be. However, `B` is not considered the
 // primary controller.
+//
+// Components `A` and `B` are wrapped in component `Wrapper`, which defaults to 'div'.
+// It may be better to make the default be the new `React.Fragment` component.
 //
 // This HOC is a special case of the React pattern "controlled component".
 // The current implementation uses a boolean state, but this could easily
 // be generalized by replacing callbacks `open` and `close` with a single
 // `setStatus` callback that sets the `show` state (better renamed `status`).
-// However, this would impose more boilerplate on the client components A
-// and B. This special case has wide application.
+// However, this would impose more boilerplate on the client components `A`
+// and `B`. This two-state special case has wide application.
 //
-// Using this HOC in real application has some subtleties. Specifically:
-//
-//  - If you want to use props (or state) in the component returned by
-//    a HOC, you have to wrap the result of the HOC in a component that
-//    defines the props (or state), and render the result of the HOC.
-//
-//  - This enables you to define components that use those props and state
-//    passed to the HOC.
-//
-//  - The components passed to the HOC must be *components*, not instantiations
-//    (objects). Typically you'll want to define functional components
-//    `A` and `B` on the class, and pass those functions as arguments to the
-//    HOC.
-//    Code fragment:
-//
-//      ButtonWithModal = buttonWithModal(this.A, this.B);
-//
-// For an example use case, see component GeoExporter.Modal.
+// For a simple example use case see component `GeoExporter`.
+// For a complex example use case see component `MapSettings`.
 
 import React from 'react';
 
@@ -60,8 +55,8 @@ export default function compAcontrolsB(A, B, Wrapper = 'div') {
     render() {
       return (
         <Wrapper>
-          <A open={this.open} close={this.close} />
-          <B show={this.state.show} open={this.open} close={this.close} />
+          <A open={this.open} close={this.close} {...this.props} />
+          <B show={this.state.show} open={this.open} close={this.close} {...this.props} />
         </Wrapper>
       );
     }
