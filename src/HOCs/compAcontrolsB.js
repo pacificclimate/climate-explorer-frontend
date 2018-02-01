319b32ab27f7054374d930fa89ff15a2866b3948
Rod Glover <rglover@uvic.ca>
2018-02-01 11:31:17 -0800

    Pass `controls` prop to A-controls-B components

--- src/HOCs/compAcontrolsB.js
+++ src/HOCs/compAcontrolsB.js
@@ -12,8 +12,17 @@
 // - All props passed to the component returned by the HOC, so that A and B can
 //    construct content based on those props, necessary for many use cases.
 //
-// - Three new values set up here: `show`, `open`, `close`, to control the
-//    behaviour of the controlled component B.
+// - Four new values set up here: `show`, `open`, `close`, `controls` to
+//    control the behaviour of the controlled component B.
+//    - The purpose and use of show`, `open`, `close` are obvious.
+//    - `controls` is an array of all the A-controls-B control props passed
+//        down through a chaining of A-controls-B components. `controls[0]`
+//        is always the local (lowest) set of controls, and `controls[i]` for
+//        `i` > 0 is the set of control props for the i-th parent of the local
+//        A-controls-B component. This allows one A-controls-B component to
+//        controlled by another, by using parent control props. (Without it,
+//        only the local control props would be available, and that is not
+//        sufficient.)
 //
 // Note: `B` (as well as `A`) is passed the state-control callbacks so that it
 // can control itself if need be. However, `B` is not considered the
@@ -30,12 +39,23 @@
 // and `B`. This two-state special case has wide application.
 //
 // For a simple example use case see component `GeoExporter`.
-// For a complex example use case see component `MapSettings`.
+// For a complex example use case, including chained A-controls-B components,
+// see component `GeoLoader`.
 
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function compAcontrolsB(A, B, Wrapper = 'div') {
+import _ from 'underscore';
+
+
+const controlPropNames = 'show open close controls'.split(' ');
+
+export default function compAcontrolsB(A, B, Wrapper = 'div', label = '') {
   return class extends React.Component {
+    static propTypes = {
+      controls: PropTypes.array,
+    };
+
     constructor(props) {
       super(props);
 
@@ -43,6 +63,8 @@ export default function compAcontrolsB(A, B, Wrapper = 'div') {
         show: false,
       };
     }
+
+    getShow = () => this.state.show;
 
     open = () => {
       this.setState({ show: true });
@@ -52,14 +74,35 @@ export default function compAcontrolsB(A, B, Wrapper = 'div') {
       this.setState({ show: false });
     };
 
+    controls() {
+      const control = [{
+        show: this.getShow,
+        open: this.open,
+        close: this.close,
+      }];
+      return control.concat(this.props.controls || []);
+    }
+
     render() {
+      const nonControlProps = _.omit(this.props, controlPropNames);
       return (
         <Wrapper>
-          <A open={this.open} close={this.close} {...this.props} />
-          <B show={this.state.show} open={this.open} close={this.close} {...this.props} />
+          <A
+            show={this.state.show}
+            open={this.open}
+            close={this.close}
+            controls={this.controls()}
+            {...nonControlProps}
+          />
+          <B
+            show={this.state.show}
+            open={this.open}
+            close={this.close}
+            controls={this.controls()}
+            {...nonControlProps}
+          />
         </Wrapper>
       );
     }
   };
 }
-
