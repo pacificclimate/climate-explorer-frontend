2eae444a95199d432c05fa01be30650ad2f113c1
Rod Glover <rglover@uvic.ca>
2018-02-01 11:31:54 -0800

    Refactor GeoLoader with A-controls-B HOC

--- src/components/GeoLoader/GeoLoader.js
+++ src/components/GeoLoader/GeoLoader.js
@@ -1,47 +1,5 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Button, Glyphicon } from 'react-bootstrap';
-import DialogWithError from './DialogWithError';
-
 import compAcontrolsB from '../../HOCs/compAcontrolsB';
+import GeoLoaderButton from './GeoLoaderButton';
+import GeoLoaderDialogWithError from './GeoLoaderDialogWithError';
 
-/*
-
-Provides a dialog to load a local spatial file.
-
-Calls callback with resulting geojson
-
-*/
-
-// TODO: Refactor as a HOC that composes the button, modal dialog, and error
-// dialog from separate components. Unfortunately we are in a bit of a hurry
-// and so we use a little cut-paste instead of the deprecated mixin.
-// See HOCs/buttonWithModal for a start on the HOC.
-
-class GeoLoader extends React.Component {
-  static propTypes = {
-    onLoadArea: PropTypes.func.isRequired,
-    title: PropTypes.string,
-  };
-
-  A = (props) =>
-    <Button onClick={props.open} title={this.props.title}>
-      <Glyphicon glyph='open-file'/>
-    </Button>
-  ;
-
-  B = (props) =>
-    <DialogWithError
-      show={props.show}
-      onHide={props.close}
-      onLoadArea={this.props.onLoadArea}
-    />
-  ;
-
-  render() {
-    const ButtonWithModal = compAcontrolsB(this.A, this.B);
-    return <ButtonWithModal/>;
-  }
-}
-
-export default GeoLoader;
+export default compAcontrolsB(GeoLoaderButton, GeoLoaderDialogWithError, 'div', 'GeoLoader');
---
+++ src/components/GeoLoader/GeoLoaderMainDialog.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FormControl, Button, Modal } from 'react-bootstrap';
+
+import g from '../../core/geo';
+
+
+export default class GeoloaderMainDialog extends React.Component {
+  // Note slightly tricky bit needed to chain A-controls-B components:
+  // This main dialog controls the error dialog. The error dialog's control
+  // props are the local ones. The control props for this (main) dialog are
+  // those of its parent (the button). We access those via the `controls`
+  // prop, specifically `controls[1]`, for the immediate parents.
+  static propTypes = {
+    show: PropTypes.bool,
+    open: PropTypes.func.isRequired,
+    close: PropTypes.func.isRequired,
+    controls: PropTypes.array,
+
+    onLoadArea: PropTypes.func.isRequired,
+  };
+
+  importPolygon = (file) => {
+    // Close the main dialog.
+    this.props.controls[1].close();
+    // Open the error dialog on fail.
+    g.load(file, this.props.onLoadArea, this.props.open);
+  };
+
+  render() {
+    return (
+      <Modal show={this.props.controls[1].show()}>
+  
+        <Modal.Header closeButton>
+          <Modal.Title>Import Polygon</Modal.Title>
+        </Modal.Header>
+  
+        <Modal.Body>
+          <FormControl
+            type='file'
+            label='Select file'
+            onChange={(e) => this.importPolygon(e.currentTarget.files[0])}
+          />
+          <p>
+            Accepts a zipped Shapefile or a single geojson Feature
+            (not FeatureCollection)
+          </p>
+        </Modal.Body>
+  
+        <Modal.Footer>
+          <Button onClick={this.props.controls[1].close}>Close</Button>
+        </Modal.Footer>
+  
+      </Modal>
+    );
+  }
+}
