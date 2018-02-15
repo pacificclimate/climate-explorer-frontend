0d95348e6c153e85182c250fcd9d339523f4a329
Rod Glover <rglover@uvic.ca>
2018-02-15 11:48:27 -0800

    Add getData to data-services/ce-backend

--- src/data-services/ce-backend.js
+++ src/data-services/ce-backend.js
@@ -28,4 +28,34 @@ function getTimeseries({ variable_id, unique_id }, area) {
   });
 }
 
-export { getTimeMetadata, getTimeseries };
+// TODO: Find a better name than 'data', really!!
+function getData(
+  { ensemble_name, model_id, variable_id, experiment, timescale, timeidx, area },
+) {
+  // Get from 'data' API endpoint.
+  // Description: Get data values from all files matching the following:
+  //  ensemble_name,
+  //  model (model_id)
+  //  variable (variable_id),
+  //  emission (experiment),
+  //  timescale,
+  //  time (timeidx)
+  //  area
+  // Those parameters are all props of components concerned, and so are
+  // grouped as a single object for convenience.
+  return axios({
+    baseURL: urljoin(CE_BACKEND_URL, 'data'),
+    params: {
+      ensemble_name: ensemble_name,
+      model: model_id,
+      variable: variable_id,
+      emission: experiment,
+      timescale: timescale,
+      time: timeidx,
+      area: area || '',
+    },
+  });
+}
+
+
+export { getTimeMetadata, getTimeseries, getData };
