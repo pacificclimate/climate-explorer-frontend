7addafa7ad53d637df5238cd132af7bd3b00951c
Rod Glover <rglover@uvic.ca>
2018-09-10 16:43:24 -0700

    Add routes
    (cherry picked from commit c033203)

--- src/components/DataTool/DataTool.js
+++ src/components/DataTool/DataTool.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
+import { Route, Redirect } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import SingleAppController from '../app-controllers/SingleAppController';
+import PrecipAppController from '../app-controllers/PrecipAppController';
+import DualAppController from '../app-controllers/DualAppController';
+import MotiAppController from '../app-controllers/MotiAppController';
 
 import _ from 'underscore';
 
@@ -37,7 +41,11 @@ export default class DataTool extends React.Component {
         </Row>
         <Row>
           <Col>
-            {this.props.children || <SingleAppController ensemble_name="ce_files"/>}
+            <Route exact path='/data' render={() => (<Redirect to='/data/climo/ce_files'/>)} />
+            <Route path='/data/moti' component={MotiAppController} />
+            <Route path='/data/climo/:ensemble_name' component={SingleAppController} />
+            <Route path='/data/compare/:ensemble_name' component={DualAppController} />
+            <Route path='/data/precipitation/:ensemble_name' component={PrecipAppController} />
           </Col>
         </Row>
       </Grid>
