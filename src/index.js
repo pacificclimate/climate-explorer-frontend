06a3722d7fd10b167d8909780a2d738af8a95120
Rod Glover <rglover@uvic.ca>
2018-09-10 16:45:31 -0700

    Add top-level nav
    (cherry picked from commit effee3e)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
-import SingleAppController from './components/app-controllers/SingleAppController';
-import DualAppController from './components/app-controllers/DualAppController';
-import MotiAppController from './components/app-controllers/MotiAppController';
-import PrecipAppController from './components/app-controllers/PrecipAppController';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
+import DataTool from './components/DataTool';
 
 class App extends React.Component {
   static propTypes = {
@@ -27,11 +23,7 @@ class App extends React.Component {
           </div>
           <div>
             <Route exact path='/' render={() => (<Redirect to='/data'/>)} />
-            <Route exact path='/data' render={() => (<Redirect to='/data/climo/ce_files'/>)} />
-            <Route path='/data/moti' component={MotiAppController} />
-            <Route path='/data/climo/:ensemble_name' component={SingleAppController} />
-            <Route path='/data/compare/:ensemble_name' component={DualAppController} />
-            <Route path='/data/precipitation/:ensemble_name' component={PrecipAppController} />
+            <Route path='/data' component={DataTool}/>
           </div>
           <div>
             <Footer />
