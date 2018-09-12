88d1a84a705d9f9f94988eab62e54ba2baff0548
Rod Glover <rglover@uvic.ca>
2018-09-11 20:39:04 -0700

    Rename SubNav -> NavRoutes

--- src/components/navigation/NavRoutes/__tests__/smoke.js
+++ src/components/navigation/NavRoutes/__tests__/smoke.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SubNav from '../SubNav';
+import NavRoutes from '../';
 import { noop } from 'underscore';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <SubNav/>,
+    <NavRoutes/>,
     div
   );
 });
