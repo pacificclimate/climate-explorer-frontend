476111ee306f8bf88915ba53e23c05091b51c6c8
Rod Glover <rglover@uvic.ca>
2018-09-11 20:18:09 -0700

    Add skeleton for full set of pages
    (cherry picked from commit 1e3e063)

---
+++ src/components/help-etc/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SubNav from '../navigation/SubNav';
+
+const navSpec = {
+  basePath: '/about',
+  items: [
+    {
+      label: 'Contact',
+      subpath: 'contact',
+      component: null,
+    },
+    {
+      label: 'Credits and Acknowledgements',
+      subpath: 'credits',
+      component: null,
+    },
+    {
+      label: 'Team',
+      subpath: 'team',
+      component: null,
+    },
+  ],
+};
+
+export default function About(props) {
+  return <SubNav { ...{ navSpec, ...props } }/>;
+}
