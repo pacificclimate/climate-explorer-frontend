aefa719b263b59a43318976cb5feae546ed4f067
Rod Glover <rglover@uvic.ca>
2018-09-13 16:03:32 -0700

    Add placeholder FAQ
    (cherry picked from commit d6d6303)

---
+++ src/components/help-etc/FAQ.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid } from 'react-bootstrap';
+import Accordion from './Accordion';
+
+
+const faqs = [
+  {
+    question: 'What is the meaning of life?',
+    answer: `
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?    
+`,
+  },
+  {
+    question: 'How can I get rich quickly?',
+    answer: `Buy low, sell high.`,
+  },
+];
+
+const items = faqs.map((faq, i) => (
+  <Accordion.Item
+    key={i}
+    eventKey={i}
+    title={`Question ${i + 1}: ${faq.question}`}
+  >
+    {faq.answer}
+  </Accordion.Item>
+));
+
+export default function FAQ() {
+  return (
+    <Grid fluid>
+      <h1>Frequently Asked Questions</h1>
+      <Accordion>
+        {items}
+      </Accordion>
+    </Grid>
+  );
+}
