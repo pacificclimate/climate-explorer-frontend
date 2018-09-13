84eb65ced5b0cbec66a900677ce63f7e2a546ac0
Rod Glover <rglover@uvic.ca>
2018-09-13 16:47:34 -0700

    Use Route render to avoid re-rendering data components on navigation

--- src/components/DataTool.js
+++ src/components/DataTool.js
@@ -13,19 +13,19 @@ const navSpec = {
       label: 'Single dataset',
       info: 'View a single climate variable from a selected GCM and emission scenario.',
       subpath: 'climo/ce_files',
-      component: SingleAppController,
+      render: () => <SingleAppController/>,
     },
     {
       label: 'Compare datasets',
       info: 'Simulataneously view and compare two climate variables from a selected GCM and emission scenario.',
       subpath: 'compare/ce_files',
-      component: DualAppController,
+      render: () => <DualAppController/>,
     },
     {
       label: 'Extreme Precipitation',
       info: 'View data representing extreme precipitation based on a selected GCM and emission scenario.',
       subpath: 'precipitation/extreme_precipitation',
-      component: PrecipAppController,
+      render: () => <PrecipAppController/>,
     },
   ],
 };
