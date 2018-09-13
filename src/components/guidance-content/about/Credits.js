3c14be4d5456cd9cd7ad9876de4aad6c2bb965ef
Rod Glover <rglover@uvic.ca>
2018-09-13 16:43:36 -0700

    Complete skeleton of guidance content
    (cherry picked from commit a1ef6ee)

---
+++ src/components/guidance-content/about/Credits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Grid } from 'react-bootstrap';
+
+export default function Credits() {
+  return (
+    <Grid fluid>
+      <h1>Credits and Acknowledgements</h1>
+      <p>Content TBD</p>
+    </Grid>
+  );
+}
