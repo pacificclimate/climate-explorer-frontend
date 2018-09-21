df018fccf0fc2a091838ff9c9e9cf75f986b467f
Rod Glover <rglover@uvic.ca>
2018-09-20 17:02:53 -0700

    Pass props thru rb-derived-components
    (cherry picked from commit 12406f2)

--- src/components/layout/rb-derived-components.js
+++ src/components/layout/rb-derived-components.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 
-export const FullWidthCol = ({ children }) =>
-  <Col lg={12} md={12} sm={12}>{children}</Col>;
+export const FullWidthCol = ({ children, ...rest }) =>
+  <Col lg={12} md={12} sm={12} {...rest}>{children}</Col>;
 
-export const HalfWidthCol = ({ children }) =>
-  <Col lg={6} md={6} sm={12}>{children}</Col>;
+export const HalfWidthCol = ({ children, ...rest }) =>
+  <Col lg={6} md={6} sm={12} {...rest}>{children}</Col>;
