856e3d3f32360c68eacf818b9bf785d4f426f848
Rod Glover <rglover@uvic.ca>
2018-10-03 15:52:16 -0700

    Fix UnfilteredDatasetsSummary smoke test

--- src/components/data-presentation/UnfilteredDatasetsSummary/__tests__/smoke.js
+++ src/components/data-presentation/UnfilteredDatasetsSummary/__tests__/smoke.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import UnfilteredDatasetsSummary from '../';
 import { noop } from 'underscore';
+import { meta } from '../../../../test_support/data';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <UnfilteredDatasetsSummary/>,
+    <UnfilteredDatasetsSummary
+      meta={meta}
+    />,
     div
   );
 });
