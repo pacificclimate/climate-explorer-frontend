6391f1f8138003cbc6f4ed817d20ed571b6268c0
Rod Glover <rglover@uvic.ca>
2018-11-29 16:54:55 -0800

    Use small buttons for GeoImport/Export tools on DataMap

--- src/components/GeoLoader/GeoLoaderButton.js
+++ src/components/GeoLoader/GeoLoaderButton.js
@@ -4,7 +4,7 @@ import { Button, Glyphicon } from 'react-bootstrap';
 
 export default function GeoloaderButton(props) {
   return (
-    <Button onClick={props.open} title={props.title}>
+    <Button bsSize='small' onClick={props.open} title={props.title}>
       <Glyphicon glyph='open-file'/>
     </Button>
   );
