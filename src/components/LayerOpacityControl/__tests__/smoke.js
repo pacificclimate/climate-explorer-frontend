37de82afc13ca8cbb0f0a9185e68cfda086f18bf
Rod Glover <rglover@uvic.ca>
2018-12-04 15:04:19 -0800

    Fix LayerOpacityControl smoke test

--- src/components/LayerOpacityControl/__tests__/smoke.js
+++ src/components/LayerOpacityControl/__tests__/smoke.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Map } from 'react-leaflet';
-import MapFaderControl from '../';
+import LayerOpacityControl from '../';
+import { noop } from 'underscore';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     div.style.height = 100;
     ReactDOM.render(
         <Map>
-            <MapFaderControl/>
+            <LayerOpacityControl
+              opacity={{
+                foo: 0.5,
+              }}
+              onChange={noop}
+            />
         </Map>,
         div);
 });
