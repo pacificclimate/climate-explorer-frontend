b0772f98f1059630c8879e6c7f2c71baf5499b5e
Rod Glover <rglover@uvic.ca>
2018-12-04 16:20:04 -0800

    Use small button size for Map Settings control

--- src/components/MapSettings/MapSettingsButton.js
+++ src/components/MapSettings/MapSettingsButton.js
@@ -5,7 +5,7 @@ import { Button, Glyphicon } from 'react-bootstrap';
 // TODO: Make into a PureComponent
 export default function (props) {
   return (
-    <Button onClick={props.open} title={props.title}>
+    <Button onClick={props.open} title={props.title} bsSize='small'>
       <Glyphicon glyph='menu-hamburger'/>
     </Button>
   );
