6cb9bc813cebe396281ee5bac7c5a64ecfa46071
Rod Glover <rglover@uvic.ca>
2018-12-06 13:54:41 -0800

    Fix StatisticalSummaryTable smoke test

--- src/components/StatisticalSummaryTable/__tests__/smoke.js
+++ src/components/StatisticalSummaryTable/__tests__/smoke.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Template from '../StatisticalSummaryTable';
+import StatisticalSummaryTable from '../StatisticalSummaryTable';
 import { noop } from 'underscore';
+import { meta } from '../../../test_support/data';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Template/>,
+    <StatisticalSummaryTable
+      model_id={'GFDL-ESM2G'}
+      variable_id={'tasmax'}
+      experiment={'historical,rcp26'}
+      meta={meta}
+    />,
     div
   );
 });
