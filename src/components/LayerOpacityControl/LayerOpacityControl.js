e5b76c93b87f5cfd7fbcc61829df13af1b3d83d9
Rod Glover <rglover@uvic.ca>
2018-12-06 16:22:04 -0800

    Enable opacity to 100%

--- src/components/LayerOpacityControl/LayerOpacityControl.js
+++ src/components/LayerOpacityControl/LayerOpacityControl.js
@@ -9,64 +9,70 @@ import StaticControl from '../StaticControl';
 import './LayerOpacityControl.css';
 
 export default class LayerOpacityControl extends PureComponent {
-    static propTypes = {
-      layerOpacity: PropTypes.object.isRequired,
-      onChange: PropTypes.func.isRequired,
+  static propTypes = {
+    layerOpacity: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showControls: false,
     };
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        showControls: false,
-      };
-    }
+  handleMouseEnter = () => this.setState({ showControls: true });
+  handleMouseLeave = () => this.setState({ showControls: false });
 
-    handleMouseEnter = () => this.setState({ showControls: true });
-    handleMouseLeave = () => this.setState({ showControls: false });
-
-    render() {
-      return (
-        <StaticControl position={'topright'}>
-          <div
-            className={'LayerOpacityControl'}
-            onMouseEnter={this.handleMouseEnter}
-            onMouseLeave={this.handleMouseLeave}
-          >
-            {
-              this.state.showControls ?
-              (
-                <Grid fluid>
-                  <Row>
-                    <Col lg={12} className='text-center'>
-                      Climate layer opacity
-                    </Col>
-                  </Row>
-                  {
-                    Object.entries(this.props.layerOpacity).map(
-                      ([layerType, opacity]) => (
-                        <Row key={layerType}>
-                          <Col lg={3}>{layerType}</Col>
-                          <Col lg={8}>
-                            <InputRange
-                              minValue={0} maxValue={1} step={0.05}
-                              formatLabel={value => `${(value*100).toFixed(0)}%`}
-                              value={opacity}
-                              onChange={this.props.onChange.bind(this, layerType)}
-                            />
-                          </Col>
-                        </Row>
-                      )
+  render() {
+    return (
+      <StaticControl position={'topright'}>
+        <div
+          className={'LayerOpacityControl'}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+        >
+          {
+            this.state.showControls ?
+            (
+              <Grid fluid>
+                <Row>
+                  <Col lg={12} className='text-center'>
+                    Climate layer opacity
+                  </Col>
+                </Row>
+                {
+                  Object.entries(this.props.layerOpacity).map(
+                    ([layerType, opacity]) => (
+                      <Row key={layerType}>
+                        <Col lg={3}>{layerType}</Col>
+                        <Col lg={8}>
+                          <InputRange
+                            // step=0.0499 ensures can go up to 100%
+                            // presumed rounding error means 0.05 won't work
+                            minValue={0} maxValue={1} step={0.0499}
+                            formatLabel={
+                              value => `${(value*100).toFixed(0)}%`
+                            }
+                            value={opacity}
+                            onChange={
+                              this.props.onChange.bind(this, layerType)
+                            }
+                          />
+                        </Col>
+                      </Row>
                     )
-                  }
-                </Grid>
-              ) : (
-                <Button bsSize='small'>
-                  <Glyphicon glyph='adjust' />
-                </Button>
-              )
-            }
-          </div>
-        </StaticControl>
-      );
-    }
+                  )
+                }
+              </Grid>
+            ) : (
+              <Button bsSize='small'>
+                <Glyphicon glyph='adjust' />
+              </Button>
+            )
+          }
+        </div>
+      </StaticControl>
+    );
+  }
 }
