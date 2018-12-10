c7d9ef763ae89dc10e1f956b9d6636b19aeb79e0
Rod Glover <rglover@uvic.ca>
2018-12-10 10:21:15 -0800

    Mock file imports for jest

---
+++ src/test_support/jest/__mocks__/fileMock.js
@@ -0,0 +1,6 @@
+// Jest mock for file imports.
+// See https://jestjs.io/docs/en/webpack#handling-static-assets
+// Reference: package.json, jest.moduleNameMapper
+
+const stub = 'test-file-stub';
+export default stub;
