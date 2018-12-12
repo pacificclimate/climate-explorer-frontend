3f987765947864e485072f39987e326b8f20be7a
Rod Glover <rglover@uvic.ca>
2018-12-12 12:11:19 -0800

    Remove debug code

--- src/components/graphs/SingleAnnualCycleGraph.js
+++ src/components/graphs/SingleAnnualCycleGraph.js
@@ -44,8 +44,6 @@ export default function SingleAnnualCycleGraph(props) {
   function dataToGraphSpec(meta, data) {
     // Convert `data` (described by `meta`) to a graph specification compatible
     // with `DataGraph`.
-    console.log('SingleACG.dataToGraphSpec, meta =', meta)
-    console.log('SingleACG.dataToGraphSpec, data =', data)
     let graph = timeseriesToAnnualCycleGraph(meta, ...data);
 
     // arrange the graph so that the highest-resolution data is most visible.
