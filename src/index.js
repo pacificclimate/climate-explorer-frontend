5fedfec979cbedc60d924d671895bfb0425df9ef
Rod Glover <rglover@uvic.ca>
2019-05-17 16:26:28 -0700

    Fix CSS includes
    (cherry picked from commit dab16c003cf5c3b0d47339c539a756059810e861)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 // import App from './App';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import 'bootstrap-slider/dist/css/bootstrap-slider.min.css';
+// import 'perfect-scrollbar/dist/css/perfect-scrollbar.min.css';
+import 'c3/c3.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
