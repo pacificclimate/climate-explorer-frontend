8e2e536b68db80eaf37db40973908f579a07e21b
Rod Glover <rglover@uvic.ca>
2019-05-21 11:32:13 -0700

    Fix failing tests
    (cherry picked from commit a135c4b33dcda41787ea0c09880c206988633665)

--- src/components/DataTable/__tests__/DataTable-test.js
+++ src/components/DataTable/__tests__/DataTable-test.js
@@ -1,10 +1,9 @@
-jest.dontMock('../DataTable');
-jest.dontMock('../../../core/util');
-
 import React from 'react';
 import TestUtils from 'react-dom/test-utils';
+import DataTable from '../DataTable';
 
-const DataTable = require('../DataTable');
+jest.dontMock('../DataTable');
+jest.dontMock('../../../core/util');
 
 describe('DataTable', function () {
   it('renders', function () {
