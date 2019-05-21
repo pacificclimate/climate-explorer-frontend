0ebafde70bf7239fdaca42ddd7d6e4727dcf0b6e
Rod Glover <rglover@uvic.ca>
2019-05-21 10:28:37 -0700

    Move map-dimensioning CSS into index.css
    (cherry picked from commit e24f9acd46ae5dff46b5b7c056cc8b20a54fb5ec)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import './index.css';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 import 'bootstrap-slider/dist/css/bootstrap-slider.min.css';
@@ -11,7 +10,7 @@ import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('wrapper'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
