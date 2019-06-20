a50354550b6aec1b9cea4bf431e4e683922762a7
Rod Glover <rglover@uvic.ca>
2019-06-20 10:15:02 -0700

    Provide external texts to App

--- src/index.js
+++ src/index.js
@@ -7,10 +7,22 @@ import 'bootstrap-slider/dist/css/bootstrap-slider.min.css';
 // import 'perfect-scrollbar/dist/css/perfect-scrollbar.min.css';
 import 'c3/c3.min.css';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+import ExternalText from './utils/external-text';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('wrapper'));
+
+const loadTexts = ExternalText.makeYamlLoader(
+  `${process.env.PUBLIC_URL}/${process.env.REACT_APP_EXTERNAL_TEXT}`
+);
+
+
+ReactDOM.render((
+  <ExternalText.Provider loadTexts={loadTexts}>
+    <App/>
+  </ExternalText.Provider>
+), document.getElementById('wrapper'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
