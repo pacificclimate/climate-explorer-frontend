670fff6c38a27f94ad41036b06c673110ea8f0e2
Rod Glover <rglover@uvic.ca>
2019-08-19 16:43:01 -0700

    Add lodash/fp mixin mapWithKey

--- src/core/lodash.mixins.js
+++ src/core/lodash.mixins.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import fp from 'lodash/fp';
 
 
 const orig = _.pick(_, ['min', 'max']);
@@ -12,20 +13,25 @@ const assert = (cond, msg) => {
 
 
 _.mixin({
-  'allDefined': (object, ...paths) =>
+  allDefined: (object, ...paths) =>
     _.every(_.pick(object, _.flatten(paths)), Boolean),
 
   // Replace _.max with a version that requires an array of numbers
-  'max': (a) => {
+  max: (a) => {
     assert(_.isArrayLike(a), '_.max(): argument is not an array');
     assert(_.every(a, _.isNumber), '_.max(): argument contains non-numbers');
     return orig.max(a);
   },
 
   // Replace _.min with a version that requires an array of numbers
-  'min': (a) => {
+  min: (a) => {
     assert(_.isArrayLike(a), '_.min(): argument is not an array');
     assert(_.every(a, _.isNumber), '_.min(): argument contains non-numbers');
     return orig.min(a);
   }
+});
+
+
+fp.mixin({
+  mapWithKey: fp.map.convert({ 'cap': false }),
 });
