6fb72e53d599a2ce5116b9cb459a35396f2e762f
Rod Glover <rglover@uvic.ca>
2019-08-22 15:18:04 -0700

    Convert PrecipAppController

--- src/components/app-controllers/PrecipAppController/PrecipAppController.js
+++ src/components/app-controllers/PrecipAppController/PrecipAppController.js
@@ -17,18 +17,16 @@
  ************************************************************************/
 
 import React from 'react';
-import createReactClass from 'create-react-class';
-import { Grid, Row, Col, Panel } from 'react-bootstrap';
+import { Grid, Row, Col, Panel, ControlLabel } from 'react-bootstrap';
+import Loader from 'react-loader';
 
-import DualDataController from '../../data-controllers/DualDataController/DualDataController';
-import Selector from '../../Selector';
-import VariableDescriptionSelector from '../../VariableDescriptionSelector';
+import DualDataController from
+    '../../data-controllers/DualDataController/DualDataController';
 import {
   modelSelectorLabel, emissionScenarioSelectorLabel, variableSelectorLabel,
-  datasetFilterPanelLabel
+  datasetFilterPanelLabel, variable1SelectorLabel
 } from '../../guidance-content/info/InformationItems';
 
-import AppMixin from '../../AppMixin';
 import g from '../../../core/geo';
 import PrecipMapController from '../../map-controllers/PrecipMapController';
 import { FullWidthCol, HalfWidthCol } from '../../layout/rb-derived-components';
@@ -38,42 +36,195 @@ import FilteredDatasetsSummary from '../../data-presentation/FilteredDatasetsSum
 import _ from 'lodash';
 import FlowArrow from '../../data-presentation/FlowArrow';
 import UnfilteredDatasetsSummary from '../../data-presentation/UnfilteredDatasetsSummary';
+import { getMetadata } from '../../../data-services/ce-backend';
+import filter from 'lodash/fp/filter';
+import find from 'lodash/fp/find';
+import flatMap from 'lodash/fp/flatMap';
+import get from 'lodash/fp/get';
+import flow from 'lodash/fp/flow';
+import flatten from 'lodash/fp/flatten';
+import map from 'lodash/fp/map';
+import reduce from 'lodash/fp/reduce';
+import assign from 'lodash/fp/assign';
+import sortBy from 'lodash/fp/sortBy';
+import {
+  EmissionsScenarioSelector,
+  ModelSelector, VariableSelector
+} from 'pcic-react-components';
 
-export default createReactClass({
-  displayName: 'PrecipAppController',
+// TODO: Extract to utility module.
+function ensemble_name(props) {
+  return (
+    (props.match && props.match.params && props.match.params.ensemble_name) ||
+    props.ensemble_name ||
+    process.env.REACT_APP_CE_ENSEMBLE_NAME
+  );
+}
 
-  /*
-   * Initial state is set after the multimeta API query run in AppMixin.componentDidMount()
-   * State provided by componentDidMount():
-   *  - model_id
-   *  - variable_id
-   *  - experiment
-   *  - meta
-   */
+export default class PrecipAppController extends React.Component {
 
-  mixins: [AppMixin],
+  // To manage fetching of metadata, this component follows React best practice:
+  // https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html#fetching-external-data-when-props-change
+  // This affects how initial state is defined (`ensemble_name`) and what
+  // is done in lifecycle hooks. The value determining whether data should
+  // be fetched is `ensemble_name(props)`. (Therefore, unlike the example in
+  // the React documentation, it not a single prop value, but it is derived
+  // directly from the props). The value managed is `this.state.meta`.
 
-  //This function filters out datasets inappropriate for this portal. A dataset
-  //the filter returns "false" on will be removed.
-  //Filters out multi-year monthly datasets; too noisy to be useful.
-  datasetFilter: function (datafile) {
-    return !(datafile.multi_year_mean === false && datafile.timescale == "monthly");
-  },
+  state = {
+    prev_ensemble_name: undefined,
+
+    model: undefined,
+    scenario: undefined,
+    variable: undefined,
+    // This is an unmitigated hack, but it is simple and leads to simple
+    // code. In this controller, `comparand` is a fixed value. Even
+    // though we never change it, it is easiest to store it here and let
+    // existing tools use the data in it, rather than coding a special solution
+    // for this case.
+    // TODO: Actually, when common code is factored out, this can probably
+    // be moved to a separate, true constant. Here for now.
+    comparand: {
+      value: {
+        representative: {
+          variable_id: 'pr',
+          variable_name: 'Precipitation',
+          multi_year_mean: true
+        }
+      }
+    },
+
+    area: undefined,  // geojson object
+    meta: null,
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    // Store prev_ensemble_name in state so we can compare when props change.
+    // Clear out previously-loaded data (so we don't render stale stuff).
+    const new_ensemble_name = ensemble_name(props);
+    if (new_ensemble_name !== state.prev_ensemble_name) {
+      return {
+        meta: null,
+        prev_ensemble_name: new_ensemble_name,
+      };
+    }
+
+    // No state update necessary
+    return null;
+  }
+
+  componentDidMount() {
+    this.fetchMetadata(ensemble_name(this.props));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.meta === null) {
+      this.fetchMetadata(ensemble_name(this.props));
+    }
+  }
+
+  fetchMetadata(ensemble_name) {
+    getMetadata(ensemble_name)
+    // Prefilter metadata to show only items we want in this portal.
+    // TODO: Extract this to a common component
+      .then(filter(
+        m => !(m.multi_year_mean === false && m.timescale === 'monthly')
+      ))
+      .then(meta => this.setState({ meta }))
+    ;
+  }
+
+  handleSetArea = (geojson) => {
+    this.setState({ area: geojson });
+  };
+
+  handleChangeModel = (model) => {
+    this.setState({ model });
+  };
+
+  // TODO: Extract (common)
+  // Default to first option if cannot be found
+  replaceInvalidModel = (options, value) => {
+    return find({ value: { representative: { model_id: 'CanESM2' }}})(options);
+  };
+
+  handleChangeScenario = (scenario) => {
+    this.setState({ scenario });
+  };
+
+  // TODO: Extract (common)
+  replaceInvalidScenario = (options, value) => {
+    return find(
+      opt => opt.value.representative.experiment.includes('rcp85')
+    )(options);
+  };
+
+  handleChangeVariable = (variable) => {
+    this.setState({ variable });
+  };
+
+  // TODO: Extract (common)
+  replaceInvalidVariable = (options, value) => {
+    const flatOptions = flatMap('options', options);
+    const option = find(this.state.comparand)(flatOptions);
+    return option;
+  };
+
+  // TODO: Factor this out (used in other components)
+  representativeValue = (optionName, valueName) => {
+    // Extract a value from the representative for a named option.
+    return get([optionName, 'value', 'representative', valueName])(this.state);
+  };
+
+  // TODO: Extract (common)
+  constraintsFor = (...optionNames) => {
+    // Returns an object containing the union of all representatives of the
+    // options named in the arguments (e.g., 'model', 'scenario').
+    // Returned object is suitable as a constraint for a
+    // `SimpleConstraintGroupingSelector`.
+    return flow(
+      flatten,
+      map(name => this.state[name]),
+      map(option => option && option.value.representative),
+      reduce((result, value) => assign(result, value), {})
+    )(optionNames)
+  };
+
+  // TODO: Extract (common)
+  filterMetaBy = (...optionNames) => {
+    // Return a filtered subset of `this.meta`, based on the selected
+    // model, emissions scenario, and variable.
+    //
+    // Initially, selectors are undefined, and go through a cascading defaulting
+    // process that eventually settles with a defined value for all of them.
+    // Returning a metadata set that is filtered by a partially settled
+    // selector set causes problems in client components. This function
+    // returns the empty array unless a full set of constraints (derived from
+    // selectors) is available.
+    const settled = _.allDefined(this.state, ...optionNames);
+    if (!settled) {
+      return [];
+    }
+    return flow(
+      filter(this.constraintsFor(...optionNames)),
+      sortBy('unique_id')
+    )(this.state.meta);
+  };
 
   // TODO: https://github.com/pacificclimate/climate-explorer-frontend/issues/122
   // TODO: https://github.com/pacificclimate/climate-explorer-frontend/issues/125
-  render: function () {
+  render() {
+    if (this.state.meta === null) {
+      return <Loader/>;
+    }
 
-    //hierarchical data selection: model, then experiment, then variable
-    const modOptions = this.getMetadataItems('model_id');
-    const expOptions = this.markDisabledMetadataItems(this.getMetadataItems('experiment'),
-        this.getFilteredMetadataItems('experiment', {model_id: this.state.model_id}));
+    const filteredMetaVariable = this.filterMetaBy('model', 'scenario', 'variable');
+    const filteredMetaComparand = this.filterMetaBy('model', 'scenario', 'comparand');
 
-    const filteredMeta = this.getFilteredMeta();
-
-    const comparand_id = 'pr';
-    const comparand_name = 'Precipitation';
-    const filteredComparandMeta = this.getFilteredMeta(comparand_id, comparand_name);
+    const model_id = this.representativeValue('model', 'model_id');
+    const experiment = this.representativeValue('scenario', 'experiment');
+    const variable_id = this.representativeValue('variable', 'variable_id');
+    const comparand_id = this.representativeValue('comparand', 'variable_id');
 
     return (
       <Grid fluid>
@@ -98,28 +249,32 @@ export default createReactClass({
               <Panel.Body>
                 <Row>
                   <Col lg={2} md={2}>
-                    <Selector
-                      label={modelSelectorLabel}
-                      onChange={this.updateSelection.bind(this, 'model_id')}
-                      items={modOptions}
-                      value={this.state.model_id}
+                    <ControlLabel>{modelSelectorLabel}</ControlLabel>
+                    <ModelSelector
+                      bases={this.state.meta}
+                      value={this.state.model}
+                      onChange={this.handleChangeModel}
+                      replaceInvalidValue={this.replaceInvalidModel}
                     />
                   </Col>
                     <Col lg={2} md={2}>
-                    <Selector
-                      label={emissionScenarioSelectorLabel}
-                      onChange={this.updateSelection.bind(this, 'experiment')}
-                      items={expOptions}
-                      value={this.state.experiment}
-                    />
+                      <ControlLabel>{emissionScenarioSelectorLabel}</ControlLabel>
+                      <EmissionsScenarioSelector
+                        bases={this.state.meta}
+                        constraint={this.constraintsFor('model')}
+                        value={this.state.scenario}
+                        onChange={this.handleChangeScenario}
+                        replaceInvalidValue={this.replaceInvalidScenario}
+                      />
                   </Col>
                   <Col lg={4} md={4}>
-                    <VariableDescriptionSelector
-                      label={variableSelectorLabel}
-                      onChange={this.handleSetVariable.bind(this, "variable")}
-                      meta={_.filter(this.state.meta, m=> {return m.variable_id != "pr"})}
-                      constraints={_.pick(this.state, "model_id", "experiment")}
-                      value={_.pick(this.state, "variable_id", "variable_name")}
+                    <ControlLabel>{variableSelectorLabel}</ControlLabel>
+                    <VariableSelector
+                      bases={this.state.meta}
+                      constraint={this.constraintsFor('model', 'scenario')}
+                      value={this.state.variable}
+                      onChange={this.handleChangeVariable}
+                      replaceInvalidValue={this.replaceInvalidVariable}
                     />
                   </Col>
                 </Row>
@@ -137,12 +292,12 @@ export default createReactClass({
         <Row>
           <FullWidthCol>
             <FilteredDatasetsSummary
-              model_id={this.state.model_id}
-              experiment={this.state.experiment}
-              variable_id={this.state.variable_id}
+              model_id={model_id}
+              experiment={experiment}
+              variable_id={variable_id}
               comparand_id={comparand_id}
-              meta={filteredMeta}
-              comparandMeta={filteredComparandMeta}
+              meta={filteredMetaVariable}
+              comparandMeta={filteredMetaComparand}
               dual
             />
           </FullWidthCol>
@@ -160,31 +315,31 @@ export default createReactClass({
         <Row>
           <HalfWidthCol>
             <PrecipMapController
-              model_id={this.state.model_id}
-              variable_id={this.state.variable_id}
-              experiment={this.state.experiment}
-              meta = {filteredMeta}
+              model_id={model_id}
+              experiment={experiment}
+              variable_id={variable_id}
+              meta={filteredMetaVariable}
               comparand_id={comparand_id}
-              comparandMeta = {filteredComparandMeta}
+              comparandMeta = {filteredMetaComparand}
               area={this.state.area}
               onSetArea={this.handleSetArea}
             />
           </HalfWidthCol>
           <HalfWidthCol>
             <DualDataController
-              ensemble_name={this.state.ensemble_name}
-              model_id={this.state.model_id}
-              variable_id={this.state.variable_id}
+              ensemble_name={ensemble_name(this.props)}
+              model_id={model_id}
+              experiment={experiment}
+              variable_id={variable_id}
+              meta={filteredMetaVariable}
               comparand_id={comparand_id}
-              experiment={this.state.experiment}
+              comparandMeta = {filteredMetaComparand}
               area={g.geojson(this.state.area).toWKT()}
-              meta = {filteredMeta}
-              comparandMeta = {filteredComparandMeta}
             />
           </HalfWidthCol>
         </Row>
       </Grid>
 
     );
-  },
-});
+  }
+}
