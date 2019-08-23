d911ae2d899b6a19d37f459ac38a723287d07fe8
Rod Glover <rglover@uvic.ca>
2019-08-23 14:50:08 -0700

    Move TimeOfYearSelector to its own directory

---
+++ src/components/TimeOfYearSelector/__tests__/smoke.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TimeOfYearSelector from '../TimeOfYearSelector';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <TimeOfYearSelector/>,
+    div
+  );
+});
