4e2a1ea2e00258cad947b57214c25bd2fe35d999
QSparks <105678101+QSparks@users.noreply.github.com>
2026-08-27 15:04:54 -0700

    Add npm policy, node 24, pin CI (#479)

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,4 +4,7 @@ set -ex
 # Convenience workspace directory for later use
 WORKSPACE_DIR=$(pwd)
 
-npm install
+sudo npm install --global npm@12
+test "$(node --version | cut -d. -f1 | tr -d v)" = "24"
+test "$(npm --version | cut -d. -f1)" = "12"
+npm ci
